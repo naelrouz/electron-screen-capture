86ba96c567f30f9dbaa4fe1635cf12869dcfa8cd
Ivan Novoselov (nael) <nael@ptburg.com>
2023-03-04 11:10:26 +0300

    first

---
+++ main.js
@@ -0,0 +1,42 @@
+// Access information about media sources that can be used to capture audio
+// and video from the desktop using the navigator.mediaDevices.getUserMedia API.
+//
+// For more info, see:
+// https://electronjs.org/docs/api/desktop-capturer
+
+const { app, BrowserWindow, desktopCapturer, screen } = require("electron");
+const path = require("path");
+let mainWindow;
+app.whenReady().then(() => {
+  mainWindow = new BrowserWindow({
+    height: 600,
+    width: 600,
+    webPreferences: {
+      nodeIntegration: false, // default in Electron >= 5
+      contextIsolation: true, // default in Electron >= 12
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  mainWindow.loadFile("index.html");
+  mainWindow.webContents.openDevTools({ mode: "detach" });
+  setTimeout(startCapture, 1000);
+});
+
+function startCapture() {
+  let allDisp = screen.getAllDisplays();
+  desktopCapturer.getSources({ types: ["screen"] }).then(async (sources) => {
+    for (const source of sources) {
+      allDisp.forEach((sc, k) => {
+        if (source.display_id == sc.id) {
+          mainWindow.webContents.send(
+            "SET_SOURCE",
+            source.id,
+            sc.scaleFactor,
+            sc.size
+          );
+        }
+      });
+    }
+  });
+}
